55de39e250183f1078b28baff538233798fd4c31
Bachitter <143877686+bachiitter@users.noreply.github.com>
2026-01-05 15:37:31 -0800

    fix type error

--- src/routes/_marketing/dogs/$dogId.tsx
+++ src/routes/_marketing/dogs/$dogId.tsx
@@ -128,7 +128,12 @@ function RouteComponent() {
         <div className="grid grid-cols-2 gap-y-8 gap-x-4">
           <StatItem label="Age" value={dog.dateOfBirth ? getAge(new Date(dog.dateOfBirth)) : "Unknown"} />
           <StatItem label="Sex" value={dog.gender ? capitalize(dog.gender) : "Unknown"} />
-          <StatItem label="Color" value={`${dog.color?.charAt(0).toUpperCase() + dog.color?.slice(1)}` || "Unknown"} />
+          <StatItem
+            label="Color"
+            value={
+              dog.color && dog.color.length > 0 ? dog.color.charAt(0).toUpperCase() + dog.color.slice(1) : "Unknown"
+            }
+          />
           <StatItem
             label="Size"
             value={dog.size ? `${capitalize(dog.size)}${dog.weight ? ` (${dog.weight} lbs)` : ""}` : "Unknown"}
